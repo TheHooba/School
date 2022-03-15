378596a19681fe69097a3f5af577cb25efd80455
TheHooba <hoobayw@gmail.com>
2022-03-15 17:36:52 +0100

    Update again
    sdojrnhlkmdfsnjkohfdsjlkmsdf

---
+++ Year 1/Security/PE/Labo 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dit is de plain text:
+            String str = "Qci cu ssg jhhnzssyq jag ssg istui qcs hgyssuzaan cu wsfaati fsi zsxoyv jag uozuicioics. Qs ysiisnu cg qsms istui mcrg jsnjagwsg qhhn agqsns ysiisnu.Qhhn wsznoct is fatsg jag ssg dnsbosgicsiazsy tag rs qsms istui isnow yssuzaan fatsg. Mh tag rs qs fssui jhhnthfsgqs ysiisnu cg qsms istui jsnjagwsg qhhn qs fssui jhhnthfsgqs ysiisn            jhywsgu qs dnsbosgicsiazsy.Qs fssui jhhnthfsgqs ysiisn cg hgms iaay cu qs ‘s’.  Rs may mcsg qai rs ugsy ssg yssuzans istui ocithfi. Ssg zcrthfsgqs jnaaw jhhn qsms hvqnaexi cu, xhs tag rs qsms jhnf jag sgenpvics uisntsn fatsg? Qs dyaw qcs zcr qsms hvqnaexi xhhni cu { Avvsyfhsu}.";
+            int lth = str.Length;
+            Console.Write(lth.ToString());
+            //Het maken van een lijst waar alle letters in staan:
+            
+            //Aanmaken variabelen om te kijken hoevaak ze gebruikt zijn:
+            int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, ee = 0, g = 0, h = 0, i = 0, j = 0,
+            k = 0, l = 0, m = 0, n = 0, o = 0, p = 0, q = 0, r = 0, s = 0, t = 0, u = 0, v = 0, w = 0,
+            x = 0, y = 0, z = 0;
+
+            //Door de text gaan om te kijken hoevaak welke letter is gebruikt.
+
+            //Uitvoer alle letters en hoevaak ze gebruikt zijn.
+
+            //Ontcijferen
+        }
+    }
+}
